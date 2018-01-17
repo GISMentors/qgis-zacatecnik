d91582fd6550c177b63a958c08dc7f33edab3e78
Martin Landa <landa.martin@gmail.com>
2018-01-17 12:24:41 +0100

    version 0.9 (QGIS 2.14)

--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@ copyright = u'2014-%d GISMentors' % get_year()
 # The short X.Y version.
 version = '0.9'
 # The full version, including alpha/beta/rc tags.
-release = '%sbeta' % version
+release = '%s' % version
 
 # -- Options for HTML output ----------------------------------------------
 
