56b9f3e01898b1bcb03cc6c47ec8b310578fd10c
Martin Landa <landa.martin@gmail.com>
2020-05-04 20:46:49 +0200

    version to 3.0

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ copyright = u'2014-%d GISMentors' % get_year()
 # built documents.
 #
 # The short X.Y version.
-version = '3.0beta'
+version = '3.0'
 # The full version, including alpha/beta/rc tags.
 release = '%s' % version
 
