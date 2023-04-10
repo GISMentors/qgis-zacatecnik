90f8a2fa2c102b73d1c6add88f48b476561c5cad
Martin Landa <landa.martin@gmail.com>
2023-04-10 21:23:18 +0200

    download template (new attemt)

--- conf.py
+++ conf.py
@@ -4,9 +4,6 @@ import sys
 import os
 
 sys.path.insert(0, os.path.join('..', 'sphinx-template'))
-print(sys.path)
-print(os.listdir('..'))
-print(os.path.abspath('..'))
 from utils import get_month_year, get_year
 
 # -- General configuration ------------------------------------------------
